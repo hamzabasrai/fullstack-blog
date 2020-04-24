a2d26a9d57399c2109349af2aa28f7ad11b00349
Hamza Basrai <hamza.basrai@gmail.com>
2020-04-23 22:08:25 -0400

    4.5 Added favouriteBlog function and test

--- tests/list_helps.test.js
+++ tests/list_helps.test.js
@@ -1,68 +1,92 @@
 const listHelper = require('../utils/list_helper');
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const listWithMultipleBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 8,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 4,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:
+      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 19,
+    __v: 0,
+  },
+];
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listWithBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 8,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 4,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 19,
-      __v: 0
-    }
-  ]
-
   test('when list has only one blog equals number of likes', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
-  })
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
 
   test('when list is empty equals zero', () => {
-    const result = listHelper.totalLikes([])
-    expect(result).toBe(0)
-  })
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
 
   test('when list has multiple blogs equals correct sum', () => {
-    const result = listHelper.totalLikes(listWithBlogs)
-    expect(result).toBe(36)
-  })
-})
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+    expect(result).toBe(36);
+  });
+});
 
+describe('favouriteBlog', () => {
+  test('when list has one blog', () => {
+    const result = listHelper.favouriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
+
+  test('when list has multiple blogs', () => {
+    const result = listHelper.favouriteBlog(listWithMultipleBlogs);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 19,
+    });
+  });
+});
 
 test('dummy returns one', () => {
   const blogs = [];
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,4 +8,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favouriteBlog = (blogs) => {
+  const blogLikes = blogs.map((blog) => blog.likes);
+  const mostLikes = Math.max(...blogLikes);
+  const index = blogLikes.indexOf(mostLikes);
+  const favourite = blogs[index];
+  
+  return {
+    title: favourite.title,
+    author: favourite.author,
+    likes: favourite.likes,
+  };
+};
+
+module.exports = { dummy, totalLikes, favouriteBlog };
