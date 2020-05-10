abe41e70ff68ebb9667ec94fe51c1a0b61f93bda
Hamza Basrai <hamza.basrai@gmail.com>
2020-05-10 14:23:21 -0400

    Added reset endpoint to backend for e2e

--- server/app.js
+++ server/app.js
@@ -34,7 +34,10 @@ app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/users', userRouter);
 app.use('/api/blogs', blogRouter);
-
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
