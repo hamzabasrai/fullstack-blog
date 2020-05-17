396391d5a1d6e9851a83774f56f2bbd9b60a945d
Hamza Basrai <hamza.basrai@gmail.com>
2020-05-17 13:26:51 -0400

    Added react router

--- blog-ui/src/index.js
+++ blog-ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -8,11 +9,13 @@ import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
